870dfef5e849cd6c82aaf369ebd23aac41226ffc
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-27 18:25:14 +0200

    add mock contract

--- deploy/00-deploy-mocks.ts
+++ deploy/00-deploy-mocks.ts
@@ -1,11 +1,26 @@
 import { network } from 'hardhat'
 import { HardhatRuntimeEnvironment, NetworkConfig } from 'hardhat/types'
 import { networkConfig, type networkConfigItem } from '../helper-hardhat.config'
+import { developmentChains } from '../helper-hardhat.config'
+
+const DECIMALS = '18'
+const INITIAL_PRICE = '2000000000000000000000' // 2000
 
 export default async function (hre: HardhatRuntimeEnvironment) {
   // @ts-ignore
   const { getNamedAccounts, deployments } = hre
   const { deploy, log } = deployments
   const { deployer } = await getNamedAccounts()
-  const chainId = network.config.chainId
+  const chainId: number = network?.config?.chainId!
+
+  if (developmentChains.includes(chainId.toString())) {
+    log('Local network detected! Deploying mocks...')
+    await deploy('MockV3Aggregator', {
+      from: deployer,
+      log: true,
+      args: [DECIMALS, INITIAL_PRICE],
+    })
+    log('Mocks deployed')
+    log('---------------------------------------------------------')
+  }
 }

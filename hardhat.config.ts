5b048c212eb9a61f0c33fa5ea3ba962ffa0f767d
Jay M Gonzalez <jaimungon@gmail.com>
2022-09-27 18:11:13 +0200

    add mock config for contract

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -16,7 +16,10 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || ''
 
 const config: HardhatUserConfig = {
-  solidity: '0.8.8',
+  // solidity: '0.8.8',
+  solidity: {
+    compilers: [{ version: '0.8.8' }, { version: '0.6.6' }],
+  },
   defaultNetwork: 'hardhat',
   networks: {
     hardhat: {

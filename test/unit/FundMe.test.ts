2f716e016aa17cb3b664d7a5b7d752f75bc67c0c
Jay M Gonzalez <jaimungon@gmail.com>
2022-10-20 10:37:47 +0200

    adding tests for fund functions

--- test/unit/FundMe.test.ts
+++ test/unit/FundMe.test.ts
@@ -8,6 +8,7 @@ describe('FundMe', function () {
   let fundMe: FundMe
   let mockV3Aggregator: MockV3Aggregator
   let deployer: SignerWithAddress
+
   beforeEach(async () => {
     if (!developmentChains.includes(network.name)) {
       throw 'You need to be on a development chain to run tests'
@@ -30,7 +31,16 @@ describe('FundMe', function () {
     it("Fails if you don't send enough ETH", async () => {
       await expect(fundMe.fund()).to.be.revertedWith('Send me more honey b***!')
     })
+    it('updates the amount funded data structure', async () => {
+      await fundMe.fund({ value: ethers.utils.parseEther('1') })
+      const response = await fundMe.addressToAmmountFunded(deployer.address)
+      assert.equal(response.toString(), ethers.utils.parseEther('1').toString())
+    })
 
-    it('It adds the sender address to the accounts array', async () => {})
+    it('It adds the sender address to funders array', async () => {
+      await fundMe.fund({ value: ethers.utils.parseEther('1') })
+      const funder = await fundMe.funders(0)
+      assert.equal(funder, deployer.address)
+    })
   })
 })
